68c84cca1fb31497865d2bd9219c96324a1acc00
Marco Torrico <marcojtorrico@gmail.com>
2025-04-19 12:58:58 -0400

    api connection

---
+++ lib/api_connection/api_connection.dart
@@ -0,0 +1,12 @@
+class API {
+  // static const hostConnect = "http://quanticasoft.com/api_logistika";
+  static const hostConnect = "http://localhost/api_logistika";
+  static const hostAdmin = "$hostConnect/admin";
+
+
+  //signUp-Login admin
+  static const validateEmail = "$hostAdmin/validate_email.php";
+  static const signUp = "$hostAdmin/signup.php";
+  static const login = "$hostAdmin/login.php";
+
+}
