4bf6a5c9994e7fb2016cb5e03527e5ff58a4d4a1
Marco Torrico <marcojtorrico@gmail.com>
2025-04-19 11:22:57 -0400

    Home & Auth Screens

--- lib/admin/screens/home/home_screen.dart
+++ lib/admin/screens/home/home_screen.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text('Login Screen'),
+        child: Text('Home Screen'),
       ),
     );
   }
---
+++ lib/admin/screens/screens.dart
@@ -0,0 +1,11 @@
+
+
+
+
+
+//home
+export 'home/home_screen.dart';
+
+//authentication
+export 'authentication/login_screen.dart';
+export 'authentication/register_screen.dart';
